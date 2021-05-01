1d4a5e8d85503732002b5de074c4dadec15852a6
Lennert Verbesselt <lennert@verbesselt.net>
2021-05-01 15:13:47 +0200

    Weird bug with Vetur fixed && Creation of vue files

---
+++ resources/js/vue/addItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="addItem">
+        <input type="text"/>
+
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
